375f131d91498fa85ebb0ee3a79153dbc6851bcd
martey <toppdogg371@gmail.com>
2023-11-18 05:37:49 -0500

    added George.tf

---
+++ George.tf
@@ -0,0 +1,10 @@
+{
+  "version": 4,
+  "terraform_version": "1.5.5",
+  "serial": 1,
+  "lineage": "5825c3b5-d13f-3114-6977-7be04af8da55",
+  "outputs": {},
+  "resources": [],
+  "check_results": null
+}
+
