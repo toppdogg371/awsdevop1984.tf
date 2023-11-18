73779f9bb168f18a198e7536c067884a114882a9
martey <toppdogg371@gmail.com>
2023-11-18 05:22:03 -0500

    user.tf

--- user.tf
+++ user.tf
@@ -1,3 +1,16 @@
-resource "aws_iam_user" "ec2-user2" {
-    name = "fmartey"
+# Terraform Block
+terraform {
+  required_version = ">= 1.5.5"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
 }
+
+# Provider Block
+provider "aws" {
+  region = "us-east-1"  
+  profile = "default"
+
